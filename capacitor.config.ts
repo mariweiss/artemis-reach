ff6443a56d6b51d711f6cfd3d3820a9d8fb1ec6f
Mari Weiss <maribweiss@gmail.com>
2026-08-17 21:56:53 -0300

    app Android funcionando com Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.artemis.reach',
+  appName: 'Artemis Reach',
+  webDir: 'out'
+};
+
+export default config;
